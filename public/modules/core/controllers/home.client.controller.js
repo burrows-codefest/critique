6b148371e0c59b83ee1b8ccbc4931b9c01f5776d
Ben Chaplin <ben.chaplin@burrows.yr.com>
2014-08-18 17:39:50 +0100

    Get git feed.

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,9 +1,18 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
-		// This provides Authentication context.
-		$scope.authentication = Authentication;
-	}
+angular.module('core').controller('HomeController', [
+  '$scope', 'Authentication', '$http',
+  function ($scope, Authentication, $http) {
+    var gitUrl = 'https://api.github.com/user/repos';
+
+    $scope.authentication = Authentication;
+
+    $scope.gitUser = 'Ben';
+
+    $http.get(gitUrl, {headers: { 'Authorization': 'token ' + $scope.authentication.user.providerData.accessToken }}).
+        success(function (data) {
+          $scope.gitUser = data;
+        });
+  }
 ]);
