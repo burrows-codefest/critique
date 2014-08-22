a26c6e370b9442a3f338122789cb0fcd288a45e8
Ben Chaplin <ben.chaplin@burrows.yr.com>
2014-08-22 16:22:11 +0100

    Fix faulty test with duff test.

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -4,9 +4,19 @@
 angular.module('core').controller('HomeController', [
   '$scope', 'Authentication', '$http',
   function ($scope, Authentication, $http) {
-    var gitUrl = 'https://api.github.com/';
+    var gitUrl = 'https://api.github.com/',
+        user = Authentication.user,
+        providerData, token;
 
-    $http.defaults.headers.common.Authorization = 'token ' + Authentication.user.providerData.accessToken;
+    if (user) {
+      providerData = user.providerData;
+
+      if (providerData) {
+        token = providerData.accessToken;
+      }
+    }
+
+    $http.defaults.headers.common.Authorization = 'token ' + token;
 
     $http.get(gitUrl + 'user/subscriptions').success(function (data) {
       $scope.repos = data;
