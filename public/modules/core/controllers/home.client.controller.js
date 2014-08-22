af17dea228e5125ded516234c8e8a7963d7494af
Ben Chaplin <ben.chaplin@burrows.yr.com>
2014-08-22 10:47:19 +0100

    Hide repos without PR's

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -6,9 +6,7 @@ angular.module('core').controller('HomeController', [
   function ($scope, Authentication, $http) {
     var gitUrl = 'https://api.github.com/';
 
-    $scope.authentication = Authentication;
-
-    $http.defaults.headers.common.Authorization = 'token ' + $scope.authentication.user.providerData.accessToken;
+    $http.defaults.headers.common.Authorization = 'token ' + Authentication.user.providerData.accessToken;
 
     $http.get(gitUrl + 'user/subscriptions').success(function (data) {
       $scope.repos = data;
--- public/modules/core/directives/showPr.client.directive.js
+++ public/modules/core/directives/showPr.client.directive.js
@@ -8,17 +8,25 @@ angular.module('core').directive('crShowPullRequests', [
       restrict: 'E',
       templateUrl: 'modules/core/directives/templates/showPullRequests.client.view.html',
       scope: {
-        'crRepo': '='
+        'crRepo': '=',
+        'crRepoShow': '='
       },
       link: function (scope) {
+
+        scope.crRepoShow = true;
+
         $http.get('https://api.github.com/repos/' + scope.crRepo + '/pulls').success(function (data) {
+          if (data.length < 1) {
+            console.log('no pull requests');
+            scope.crRepoShow = false;
+          }
+
           scope.pullRequests = data;
 
           for (var i = 0; i < data.length; i += 1) {
             if (data[i].comments_url) {
               $http.get(data[i].comments_url).success(function (comments) {
                 scope.pullRequests.comments = comments;
-                console.log(comments);
               });
             }
           }
